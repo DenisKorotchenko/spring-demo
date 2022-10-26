c4547b670b37b96c048ae8b4ddf5c61b4a2a10bd
DenisKorotchenko <dzd1024ozd@gmail.com>
2022-10-26 17:31:27 +0300

    Add springframework plugin

--- adulthood/build.gradle.kts
+++ adulthood/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins{
     kotlin("jvm")
     `maven-publish`
+    id("org.springframework.boot") version "2.6.4"
 }
 
 repositories {
--- adulthood/src/main/kotlin/denis/korotchenko/adulthood/AdulthoodAutoConfiguration.kt
+++ adulthood/src/main/kotlin/denis/korotchenko/adulthood/AdulthoodAutoConfiguration.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableAutoConfiguration
 open class AdulthoodAutoConfiguration {
 
     @Bean
