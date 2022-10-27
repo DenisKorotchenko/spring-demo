1bb0a9806076776738584f4a3908bb544d29c867
DenisKorotchenko <dzd1024ozd@gmail.com>
2022-10-27 08:25:56 +0300

    Update auto-config

--- adulthood/src/main/kotlin/denis/korotchenko/adulthood/AdulthoodAutoConfiguration.kt
+++ adulthood/src/main/kotlin/denis/korotchenko/adulthood/AdulthoodAutoConfiguration.kt
@@ -1,12 +1,10 @@
 package denis.korotchenko.adulthood
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableAutoConfiguration
 open class AdulthoodAutoConfiguration {
 
     @Bean
--- src/main/kotlin/denis/korotchenko/demo/controller/DemoController.kt
+++ src/main/kotlin/denis/korotchenko/demo/controller/DemoController.kt
@@ -1,7 +1,6 @@
 package denis.korotchenko.demo.controller
 
 import denis.korotchenko.adulthood.AdulthoodChecker
-import org.springframework.context.ApplicationContext
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
